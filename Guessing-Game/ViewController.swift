06fab2629344629afd964f9654b9c188f270c470
Matthew Riley <matthewriley@interapts-macbook-pro-10.local>
2018-09-24 13:44:59 -0500

    1. In Main.storyboard: 	A. Created all buttons, labels, and labels 	B. Connect all under UIViewController as IBOutlets 	C. Connected both buttons as IBActions

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var attemptsLabel: UILabel!
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    @IBOutlet weak var guessingStatusLabel: UILabel!
+    
+    @IBOutlet weak var userInputTextField: UITextField!
+    
+    @IBOutlet weak var resetButtonText: UIButton!
+   
+    @IBOutlet weak var submitButtonText: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
 
