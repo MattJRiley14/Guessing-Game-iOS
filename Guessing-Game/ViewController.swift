a54553146931bae000a8eae49e5dd118ce44b546
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-09-25 12:44:59 -0500

    In ViewController File: 	A. Created and connected timesPlayedLabel 	B. Gave greetingLabel, userInputTextField, guessingStatusLabel, attemptsLabel, and timesPlayedLabel String values in viewDidLoad function 	C. Created the following variables: randomNumber, numberOfTries, guessedNumber, timesPlayed 	D. Entered everything in resetButtonTapped function 	E. Entered everything in submitButtonTapped function

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -11,34 +11,88 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var attemptsLabel: UILabel!
-    
     @IBOutlet weak var greetingLabel: UILabel!
-    
     @IBOutlet weak var guessingStatusLabel: UILabel!
-    
     @IBOutlet weak var userInputTextField: UITextField!
-    
     @IBOutlet weak var resetButtonText: UIButton!
-   
     @IBOutlet weak var submitButtonText: UIButton!
-    
-    
-    
+    @IBOutlet weak var timesPlayedLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-    }
-
-    @IBAction func resetButtonTapped(_ sender: Any) {
-        
+        greetingLabel.text = "Welcome to the Guessing Game"
+        userInputTextField.text = "Enter guess here"
+        guessingStatusLabel.text = "Guess a number between 1 and 100"
+        attemptsLabel.text = "Attempts left: 5"
+        timesPlayedLabel.text = ""
     }
     
+    var randomNumber = Int(arc4random_uniform(100)) + 1
+    var numberOfTries = 5
+    var guessedNumber: Int? = nil
+    var timesPlayed = 0
+    
+
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        randomNumber = Int(arc4random_uniform(100)) + 1
+        numberOfTries = 5
+        userInputTextField.text = "Enter guess here"
+        guessingStatusLabel.text = "Guess a number between 1 and 100"
+        guessedNumber = nil
+        attemptsLabel.text = "Attempts left: \(numberOfTries)"
+        timesPlayed += 1
+        timesPlayedLabel.text = "Number of times you have played: \(timesPlayed)"
+    }
     
     @IBAction func submitButtonTapped(_ sender: Any) {
+        if userInputTextField.text == "Show Answer" {
+            guessingStatusLabel.text = "The number is: \(String(randomNumber))"
+            return
+        }
+        
+        if let textFieldAsANumber = Int(userInputTextField.text!) {
+            if textFieldAsANumber >= 1 && textFieldAsANumber <= 100 {
+                guessedNumber = textFieldAsANumber
+            } else {
+                guessingStatusLabel.text = "Please enter a number between 1 and 100"
+                guessedNumber = nil
+                return
+            }
+        } else {
+            guessingStatusLabel.text = "Invalid entry. Please enter a number"
+            guessedNumber = nil
+            return
+        }
+        
+        guard let guessedNumber = guessedNumber else {
+            return
+        }
+
+        if guessedNumber == randomNumber {
+            guessingStatusLabel.text = "YOU WON!!!"
+        } else if guessedNumber > randomNumber {
+            numberOfTries -= 1
+            guessingStatusLabel.text = "Guess Lower"
+            if numberOfTries >= 0 {
+                attemptsLabel.text = "Attempts left: \(numberOfTries)"
+            }
+        } else if guessedNumber < randomNumber {
+            numberOfTries -= 1
+            guessingStatusLabel.text = "Guess Higher"
+            if numberOfTries >= 0 {
+                attemptsLabel.text = "Attempts left: \(numberOfTries)"
+            }
+        }
+        
+        if numberOfTries <= 0 {
+            guessingStatusLabel.text = "GAME OVER!!! The answer was \(randomNumber)"
+        }
         
     }
     
 }
+
+
 
